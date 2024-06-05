9962d987b26f597aae1ffd71d07d041644a66cb5
Hugo Dias <116220916+hugodiasUM@users.noreply.github.com>
2024-06-05 21:20:56 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,6 +17,22 @@ router.get('/main', (req, res) => {
   res.render('main');
 });
 
+router.get('/listaRecursos', (req, res) => {
+  res.render('listaRecursos');
+});
+
+router.get('/listaPosts', (req, res) => {
+  res.render('listaPosts');
+});
+
+router.get('/rankings', (req, res) => {
+  res.render('rankings');
+});
+
+router.get('/adicionarRecurso', (req, res) => {
+  res.render('addRec');
+});
+
 router.get('/perfil', (req, res) => {
   res.render('perfil');
 });
