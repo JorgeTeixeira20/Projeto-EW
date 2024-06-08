1f2927f028a458e883d17b0fc21d408a4d8998d5
Hugo Dias <116220916+hugodiasUM@users.noreply.github.com>
2024-06-08 23:12:15 +0100

    Update resource.js

--- src/models/resource.js
+++ src/models/resource.js
@@ -5,19 +5,25 @@ const fileSchema = new mongoose.Schema({
   filePath: { type: String, required: true }
 });
 
-const rankingSchema = new mongoose.Schema({
+const reviewSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   stars: { type: Number, required: true }
 });
 
 const resourceSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  type: { type: String, required: true },
   title: { type: String, required: true },
+  subtitle: { type: String, default: '' },
+  description: { type: String, required: true },
+  dataCriacao: { type: Date, required: true },
+  dataRegisto: { type: Date, required: true },
+  visibilidade: { type: String, required: true },
   author: { type: String, required: true },
   year: { type: Number, required: true },
-  type: { type: String, required: true },
   themes: [{ type: String, required: true }],
   files: [fileSchema],
-  rankings: [rankingSchema]
+  reviews: [reviewSchema]
 });
 
 module.exports = mongoose.model('Resource', resourceSchema);
